60f4c7f9e4c89099c820fd8c95f782562a784f5c
Blake Eriks <eblake98@vt.edu>
2021-10-30 11:25:31 -1000

    Complete you_know_xor_you_dont

---
+++ cryptohack/XOR/you_know_xor_you_dont.py
@@ -0,0 +1,21 @@
+# I've encrypted the flag with my secret key, you'll never be able to guess it.
+
+# Remember the flag format and how it might help you in this challenge!
+
+import pwn
+
+encoded = '0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104'
+
+encoded_bytes = bytes.fromhex(encoded)
+
+flag_bytes = 'crypto{'.encode()
+
+key_part = pwn.xor(flag_bytes, encoded_bytes[:7]).decode()
+key_part += 'y'
+
+key_part = key_part * 5 + key_part[:2]
+print('Flag is: ' + pwn.xor(key_part.encode(), encoded_bytes[:len(key_part)]).decode())
+
+# for i in range(50):
+#     print(pwn.xor(bytes.fromhex(encoded), flag_bytes).decode('utf-8'))
+#     flag_bytes += 'x';
